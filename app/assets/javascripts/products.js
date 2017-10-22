ec491f4da46757e5b43f73bdc18ffe96eb15a94c
carterfiber <gerald@carterfiber.com>
2017-10-22 18:14:59 -0400

    cancan ability tweak, rm dupl notice, styled upload view

--- app/assets/javascripts/products.js
+++
@@ -1,6 +0,0 @@
-// hide alerts after a specified time
-// $(function() {
-//    $('#notice').delay(1500).fadeIn('normal', function() {
-//       $(this).delay(3000).fadeOut();
-//    });
-// });
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,21 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+    user ||= User.new # guest user (not logged in)
+
     if user.admin?
         can :manage, :all
-    # elsif user.seller?
-    #     can :manage, :Product
-    # elsif user.buyer?
-    #     can :read, :all
+
     elsif user.guest?
-        can :show, :Product
+        can :show, :all
     else
         can :read, :all
     end
